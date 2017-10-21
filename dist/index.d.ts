f385bce725c0c2fe8b7d57a4cfd95ccbadd6533c
CJ Johnson <me@cj-johnson.com>
2017-10-21 17:23:37 -0400

    Cleanup

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Bullseye from './components/composable/Bullseye';
-import Layer from './components/composable/Layer';
-import Group from './components/composable/Group';
-import Path from './components/primitive/Path';
-export default Bullseye;
-export { Layer, Group };
-export { Path };
