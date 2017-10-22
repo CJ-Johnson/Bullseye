b70bafb7c3aa8949e7d6ddc83af3d301d68e876d
CJ Johnson <me@cj-johnson.com>
2017-10-22 16:22:50 -0400

    Add drawOnCanvas

--- src/main/targets/CanvasTarget/index.tsx
+++ src/main/targets/CanvasTarget/index.tsx
@@ -5,9 +5,18 @@ import {
   RenderingTargetArguments,
 } from '../../../main'
 
+const drawOnCanvas = (args: RenderingTargetArguments, canvas: HTMLCanvasElement): void => {
+  const context = canvas.getContext('2d')
+  if (!context) {
+    throw new Error('Unable to load 2d context from provided canvas element')
+  }
+  context.fillStyle = 'rgb(0,0,0)'
+  context.fillRect(10, 20, 255, 150)
+}
+
 const CanvasTarget: RenderingTarget = (args: RenderingTargetArguments) => (
   <canvas
-    ref={(...args: any[]) => console.log('Canvas is on the page!', {args})}
+    ref={(canvas: HTMLCanvasElement): void => drawOnCanvas(args, canvas)}
     width={args.width}
     height={args.height}
   >
