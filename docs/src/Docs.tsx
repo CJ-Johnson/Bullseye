08dd6584646b976714894b53afcfb98a14ae3da2
CJ Johnson <me@cj-johnson.com>
2017-10-21 22:22:00 -0400

    Saving for eject

--- docs/src/Docs.tsx
+++ docs/src/Docs.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import './Docs.css';
-
 const logo = require('./logo.svg');
+
+import Bullseye from '../..'
+
 
 class Docs extends React.Component {
   render() {
+    if (window) {
+      return (
+        <Bullseye
+          target={(...args: any[]) => console.log('called target with ', {args})}
+          width={500}
+          height={500}
+        />
+      )
+    }
     return (
       <div className="root">
         <div className="header">
